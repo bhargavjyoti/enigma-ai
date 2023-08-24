ede8f1d78717986699aba82da5cc8dbc21e0c06f
Bhargav Kumar <champion.maverick360@gmail.com>
2023-08-24 13:40:41 +0530

    remove go back button from 404 page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -7,9 +7,6 @@ const notfound = () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
         <Image src={errImg} alt="404 Image" width={400} height={400} />
-        <Link href="/dashboard">
-            <Button variant="outline" className="md:text-lg p-4 md:p-6 rounded-full font-semibold text-white bg-[#dd720e]">Go To Homepage</Button>
-        </Link>
     </div>
   )
 }
